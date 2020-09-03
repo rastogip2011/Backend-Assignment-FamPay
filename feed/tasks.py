c44b7b28832fb75d4f5ddf8576b281aefe0c249e
prashant.rastogi.eee16 <prashant.rastogi.eee16@itbhu.ac.in>
2020-09-03 10:46:51 +0530

    Automatic Key usage

--- feed/tasks.py
+++ feed/tasks.py
@@ -8,20 +8,30 @@ def populate_db():
 
     keys = get_key()
     Feed.objects.all().delete()
-    link = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=100&order=date&q=football&key='+keys[0]
+    link = 'https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=100&order=date&q=football&key='
 
-    data = requests.get(link).json()
-    for feed in data['items']:
-        Vid_id = feed['id']['videoId']
-        snippet = feed['snippet']
-        Title = snippet['title']
-        Published_at = snippet['publishedAt']
-        Description = snippet['description']
-        Thumbnail = snippet['thumbnails']['medium']
-        Thumbnail_url = Thumbnail['url']
+    for key in keys:
+
+        response = requests.get(link+key)
         
-        new_feed = Feed(vid_id=Vid_id, title=Title, description=Description, published_at=Published_at, thumbnail_url=Thumbnail_url)
-        new_feed.save()
+        if str(response) == '<Response [403]>':
+            continue
+        
+        data = response.json()
+
+        for feed in data['items']:
+            Vid_id = feed['id']['videoId']
+            snippet = feed['snippet']
+            Title = snippet['title']
+            Published_at = snippet['publishedAt']
+            Description = snippet['description']
+            Thumbnail = snippet['thumbnails']['medium']
+            Thumbnail_url = Thumbnail['url']
+            
+            new_feed = Feed(vid_id=Vid_id, title=Title, description=Description, published_at=Published_at, thumbnail_url=Thumbnail_url)
+            new_feed.save()
+        
+        break
 
 def get_key():
     key_file = open('keys.txt')
